4a016b25f87c53edddba8cd49f5d00adbfa4df70
alexbotha <alexbotha@hotmail.co.uk>
2022-11-29 13:56:49 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+import ToyCard from "./ToyCard";
+
+const API = "http://localhost:3001/toys";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((data) => {
+        setToys(data);
+      });
+  }, []);
+
+  function updatingToylist(newToyDetails) {
+    setToys([...toys, newToyDetails]);
+  }
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -14,11 +30,11 @@ function App() {
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm updatingToylist={updatingToylist} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
-  return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+function ToyContainer({ toys }) {
+  const toy = toys.map((toy) => <ToyCard toy={toy} />);
+
+  return <div key={toy.id}>{toy}</div>;
 }
 
 export default ToyContainer;
